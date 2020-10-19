4659c4bc8f5378d3f82157487a908f7525a25f17
CurryGod56 <ronith.bokkisam@gmail.com>
2020-10-19 12:42:29 -0400

    Debugging Angry Birds Code

--- sketch.js
+++ sketch.js
@@ -82,7 +82,7 @@ function draw(){
 
 function mouseDragged(){
     //if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+        Matter.Body.setPosition(bird.body, {x: mouseX, y: mouseY});
     //}
 }
 
@@ -94,7 +94,10 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-       slingshot.attach(bird.body);
+        Matter.Body.setPosition(bird.body,{x:200, y:50});
+        slingshot.attach(bird.body);
+        bird.trajectory = [];
+        
     }
 }
 
